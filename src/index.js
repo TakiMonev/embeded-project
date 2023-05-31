e9557ac8f1627a707bb591a25ef1bdb1a1c68555
SangYoun <lsyun1234@naver.com>
2023-05-31 22:58:22 +0900

    crud

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+const React = require('react');
+const ReactDOM = require('react-dom');
+const App = require('./App');
 
 ReactDOM.render(
-  <App />,
+  App.default ? <App.default /> : <App />,
   document.getElementById('root')
 );

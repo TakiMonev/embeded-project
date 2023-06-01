9cd1cd7f1feb375737e0f509fec4d4f364891cc6
TakiMonev <61460821+TakiMonev@users.noreply.github.com>
2023-06-01 16:00:39 +0900

    Delete config.php

--- src/php/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$config = array(
-    "db_host" => "mydb.c4yxzoa8i9pa.ap-northeast-2.rds.amazonaws.com",
-    "db_user" => "admin",
-    "db_password" => "!Vcd7xkzl1",
-    "db_name" => "mydb"
-);
-?>
